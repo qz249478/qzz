07d6148e93830f664f5627282c7ecbfa4211ccc5
qzz <1654455431@qq.com>
2019-05-09 18:29:30 +0800

    tset commit

---
+++ MyServelet/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        if (Float.valueOf("0.5")*10000==Float.valueOf("5000.00")){
+            System.out.print("ss");
+        }
+        System.out.print("aa");
+    }
+}
